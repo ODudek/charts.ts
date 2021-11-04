91e6a4867de5eaeacfb5183c02ef89c03a631582
Aleksander Dudek <dudek.olek4@gmail.com>
2021-11-04 18:49:51 +0100

    fix popover styles

--- src/core/popover.ts
+++ src/core/popover.ts
@@ -13,11 +13,12 @@ export class Popover {
     create(width: string, x: string, y: string) {
         this.element = document.createElement('div');
         this.element.classList.add('popover');
+        this.element.setAttribute('style', `--top: ${y}px`)
         this.element.style.opacity = '0';
         this.element.style.width = width;
         this.element.style.left = x + 'px';
-        this.element.style.top = y + 'px';
-        this.element.innerHTML = `<p>${this.title} - ${this.value}</p>`;
+        this.element.setAttribute('data-top', y);
+        this.element.innerHTML = `<span class="popover-arrow"></span><p>${this.title} - ${this.value}</p>`;
         setTimeout(() => {
             if (this.element) {
                 this.element.style.opacity = '1';
